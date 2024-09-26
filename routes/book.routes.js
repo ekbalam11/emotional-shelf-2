621b56d09b43fcce4cef6f821af23eb7c33f377c
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2024-09-26 12:26:32 -0600

    routes config

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { getBooks, getRecommendactionsByEmotion } = require('../controllers/book.controllers');
 const router = express.Router();
 
+router.get('/books', getBooks);
+router.get('/books/recommendations/:emotion', getRecommendactionsByEmotion)
 
 module.exports = router;
