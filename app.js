93e718db4bb52a59289776057ac0d358ba8ddcf4
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2024-09-26 12:05:22 -0600

    PORT config

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 const connectDB = require('./config/db'); //en este caso la configuración de la base de datos se hará en un archivo de la carpeta config
 const bookRoutes = require('./routes/book.routes')
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+})
