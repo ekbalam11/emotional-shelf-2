06a778393da94ff06180aeaa2524b84429ad2a62
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2024-09-26 13:54:08 -0600

    iteration 3 done: random book by emotion.

--- controllers/book.controllers.js
+++ controllers/book.controllers.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book.model');
+const { validationResult } = require('express-validator')
 
 exports.getBooks = async (req, res) => {
     const books = await Book.find().limit(20);
@@ -14,7 +15,7 @@ exports.getRecommendactionsByEmotion = async(req, res) => {
     // ¿Cómo obtener el valor dinámico ":emotion" de /api/books/recommendations/:emotion
     const { emotion } = req.params;
 
-    const error = validationResult(req);
+    const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
         return res.status(400).json({
@@ -30,7 +31,7 @@ exports.getRecommendactionsByEmotion = async(req, res) => {
         emotions: { $in: [emotion] }
     });
 
-    console.log('books: ', books[0]);
+    console.log('books 1-3:', books.slice(0, 3));
     
 
     //3. Responder al cliente con un JSON con una respuesta similar a la del controlador getBooks en cuanto a su estructura de respuesta
@@ -39,4 +40,34 @@ exports.getRecommendactionsByEmotion = async(req, res) => {
         results: books
     })
 
+}
+
+    exports.getRandomRecommendationByEmotion = async (req, res) => {
+        const { emotion } = req.params;
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                message: `error when trying to find random books with emotion ${emotion}`,
+                errors: errors.array()
+            })
+        }
+     
+        //Incluir expresión regular para que no distinga entre mayus y minus
+    
+        //2. Usar el ":emotion" para hacer una búsqueda en el modelo de los 20 primeros libros que incluyen la emoción ":emotion"
+        const randomBook = await Book.aggregate([
+            { $match: { emotions: { $in: [emotion] } } }, 
+            { $sample: { size: 1 } }// Randomly select one book from the matched books
+        ]);
+    
+        console.log('randomBook:', randomBook);
+        
+    
+        //3. Responder al cliente con un JSON con una respuesta similar a la del controlador getBooks en cuanto a su estructura de respuesta
+        res.status(200).json({
+            message: "Query executed successfully for emotion "+ emotion,
+            results: randomBook
+        })
 }
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getBooks, getRecommendactionsByEmotion } = require('../controllers/book.controllers');
+const { getBooks, getRecommendactionsByEmotion, getRandomRecommendationByEmotion } = require('../controllers/book.controllers');
+const { validateEmotion } = require('./book.validations');
+
 const router = express.Router();
 
-const allowedEmotions = ['Inspiration', 'Curiosity', 'Escapism', 'Nostalgia', 'Happiness', 'Sadness'];
 
 router.get('/books', getBooks);
-router.get('/books/recommendations/:emotion', getRecommendactionsByEmotion)
-
+router.get('/books/recommendations/:emotion', validateEmotion, getRecommendactionsByEmotion);
+router.get('/books/recommendations/:emotion/random', validateEmotion, getRandomRecommendationByEmotion)
 module.exports = router;
