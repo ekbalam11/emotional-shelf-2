eb450a36ab1ab1b85f1ace7ab9e7ea7b01abc7dd
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2024-09-26 12:07:19 -0600

    controller creation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ dotenv.config();
 const connectDB = require('./config/db'); //en este caso la configuración de la base de datos se hará en un archivo de la carpeta config
 const bookRoutes = require('./routes/book.routes')
 
-
+app.use('/api', bookRoutes);
 
 
 const PORT = process.env.PORT || 3000;
